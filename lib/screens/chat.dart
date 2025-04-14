340a518a33070f391009ee7c918a69253b629ceb
Laurie Domino <domino.laurie@gmail.com>
2025-04-14 09:39:53 -0500

    Added chat messages and submitting, and list of chat messages

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,31 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+class _ChatScreenState extends State<ChatScreen> {
+  
+ void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +38,18 @@ class ChatScreen extends StatelessWidget {
               FirebaseAuth.instance.signOut();
             },
             icon: Icon(
-              Icons.logout,
+              Icons.exit_to_app,
               color: Theme.of(context).colorScheme.primary,
             ),
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Logged in!'),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages()), 
+            NewMessage(),
+        ],
       ),
     );
   }
